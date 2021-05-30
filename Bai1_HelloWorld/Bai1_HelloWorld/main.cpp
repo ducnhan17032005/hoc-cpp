7dec62b4ce8710494314423985c6961c29804d21
ducnhan17032005 <85066464+ducnhan17032005@users.noreply.github.com>
2021-05-31 01:30:00 +0700

    Thu nghiem coide moi

--- Bai1_HelloWorld/Bai1_HelloWorld/main.cpp
+++ Bai1_HelloWorld/Bai1_HelloWorld/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <string>
 using namespace std;
 //entry point
+#include <math.h>
 
-int main() {
-	cout << "Hello world" << endl; 
-	return 0;
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    long themang, n;
+    int sochuso;
+
+    cout << "Nhap n: ";
+    cin >> n;
+
+    sochuso = 0;
+    themang = n;
+
+    if (n == 0)
+        sochuso = 1;
+    //n = 1234
+    //n = 123 scs  +=1 = 1
+    //n = 12  scs +=1  = 2
+    //n=1      scs +=1 =3
+    // n=0    scs +=1 =4
+    //
+    while (themang != 0)
+    {
+        sochuso = sochuso + 1;
+        themang = themang / 10;
+    }
+    printf("\nSo chu so cua %ld la %d", n, sochuso);
+
+
+    getch();
+    return 0;
 }
