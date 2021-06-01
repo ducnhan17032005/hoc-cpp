0ad011cc7d418f7e1352e5e4155adb22052d7ba8
DucNhan <tranductb100@gmail.com>
2021-06-01 23:50:10 +0700

    (no branch): Auto stash before checking out "HEAD"

--- Bai1_HelloWorld/Bai1_HelloWorld/main.cpp
+++ Bai1_HelloWorld/Bai1_HelloWorld/main.cpp
@@ -2,7 +2,9 @@
 #include <string>
 using namespace std;
 
-
+/// <summary>
+/// Bai 1
+/// </summary>
 void ex1_1() {
 	int i, n, s;
 	s = 0, i = 1;
@@ -29,10 +31,25 @@ void ex1_2() {
 	cout << "s bang: " << s << endl;
 }
 
+/// <summary>
+/// bai 2
+/// </summary>
+void ex2() {
+	int i, n, s;
+	s = 0, i = 1;
+
+	cout << "n = ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		s = s + pow(i, 2);
+	}
+	cout << "s bang: " << s << endl;
+}
 
 int main()
 {
 	//S= 1+ 2+ 3 +4 +5 + ... + n
-	ex1_2();
 	system("pause");
 }
