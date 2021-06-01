83bc83c713b30476b217517bcd7012cff0c5faef
DucNhan <tranductb100@gmail.com>
2021-06-02 00:38:16 +0700

    [bai19] 2 vong lap long nhau, giai thua

--- Bai1_HelloWorld/Bai1_HelloWorld/main.cpp
+++ Bai1_HelloWorld/Bai1_HelloWorld/main.cpp
@@ -65,6 +65,125 @@ void ex3() {
 	}
 	cout << "s bang: " << s << endl;
 }
+void ex4_sub() {
+	//s = 1*2*3 .... * n 
+	// i =1 , s=1 *1
+	// i= 2 , s = 1 * 1 * 2  = s *i
+	// i = 3, s= 1* 1  * 2 *3 = s * i
+	int n= 5;
+	int s = 1; 
+
+	for (int i = 1;i <= n; i++) 
+	{
+		s = s * i;
+	}
+}
+void ex4() {
+	int n;
+	int s = 0;
+	// s=  1 + 1*2 + 1*2*3 + .... + 1*2.... *n
+	cout << "n = ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int temp = 1;
+		//tinh temp
+		for (int j = 1; j <= i; j++) {
+			temp = temp * j;
+		}
+		//
+		s = s + temp;
+	}
+	cout << "s bang: " << s << endl;
+}
+
+
+
+void ex5() {
+	int n;
+	double s = 0;
+	// s=  1/1 + 1/1+2 + 1/1+2+3 + .... + 1/1+2.... +n
+	cout << "n = ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int temp = 1;
+		//tinh temp
+		for (int j = 1; j <= i; j++) {
+			temp = temp + j;
+		}
+		//
+		s = s +1/(double) temp;
+	}
+	cout << "s bang: " << s << endl;
+}
+
+
+void ex5() {
+	int n;
+	double s = 0;
+	// s=  1/1 + 1/1+2 + 1/1+2+3 + .... + 1/1+2.... +n
+	cout << "n = ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int temp = 1;
+		//tinh temp
+		for (int j = 1; j <= i; j++) {
+			temp = temp + j;
+		}
+		//
+		s = s + 1 / (double)temp;
+	}
+	cout << "s bang: " << s << endl;
+}
+
+void ex6() {
+	int n;
+	double s = 0;
+	// s=  1/1! + 1/2! + 1/3! + .... + 1/n! 
+	cout << "n = ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int temp = 1;
+		//tinh temp
+		for (int j = 1; j <= i; j++) {
+			temp = temp * j;
+		}
+		//
+		s = s + 1 / (double)temp;
+	}
+	cout << "s bang: " << s << endl;
+}
+
+
+void ex7() {
+	int n;
+	double s = 0;
+	// s=  1 , s +  x^1/1! + x^3/3! + x^5/5! +.,...  + x^(2n+1)/(2n+1)!
+	cout << "n = ";
+	cin >> n;
+
+	for (int i = 1; i <= 2*n+1; i = i+2)
+	{
+		
+		int temp = 1;
+		//tinh temp
+
+		for (int j = 1; j <= i; j++) {
+			temp = temp * j;
+		}
+		//
+		s = s + 1 / (double)temp;
+	}
+	cout << "s bang: " << s << endl;
+}
+
 
 
 
